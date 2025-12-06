3d2646ca98244c72bc71d4501991a269d78a925e
tugbaolcer <tugbaolcer52@gmail.com>
2025-12-06 11:42:11 +0300

    build: Configure root build.gradle.kts plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
     namespace = "com.tugbaolcer.clonex"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.tugbaolcer.clonex"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
